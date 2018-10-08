78a04f3ece27f4eddfb58de024789935389cefb0
Yuki Hattori <yukihattori1116@gmail.com>
2018-10-09 00:32:01 +0900

    Add style support for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,32 @@
-module.exports = {
-  entry: {
-    index: './index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]s$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = env => {
+  const production = !!(env || {}).production
+
+  return {
+    entry: { index: './index.js' },
+    mode: production ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.[tj]s$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            production ? MiniCssExtractPlugin.loader : 'style-loader',
+            { loader: 'css-loader', options: { importLoaders: 2 } },
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  }
 }
