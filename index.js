771ab92e36c2e9c70810155606cac0bbdefdcabf
Yuki Hattori <yukihattori1116@gmail.com>
2018-10-14 03:28:28 +0900

    Fix reflow stacking and blinking to resize by using upgraded marp-core

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import './style/index.scss'
-import '@marp-team/marp-core/lib/browser'
 import index from './src/'
 
 index()
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import MarpBrowser from '@marp-team/marp-core/lib/browser.cjs'
 import IncrementalDOM from 'incremental-dom'
 import { patch } from './marp/incremental-dom-proxy'
 import MarpManager from './marp/manager'
 
 export default function index() {
+  let updates: (() => void)[] = []
+
+  const animationFrame = () => {
+    for (const u of updates) u()
+    updates = []
+
+    MarpBrowser(false)
+    window.requestAnimationFrame(animationFrame)
+  }
+
   const marpMg = new MarpManager()
   const editor = <HTMLTextAreaElement>document.getElementById('editor')
   const preview = <HTMLDivElement>document.getElementById('preview')
@@ -14,7 +25,7 @@ export default function index() {
     const { html, css } = rendered
 
     const renderDOM = () =>
-      window.requestAnimationFrame(() => {
+      updates.push(() => {
         if (previewCSS.textContent !== css) previewCSS.textContent = css
         patch(IncrementalDOM, preview, html)
       })
@@ -28,4 +39,7 @@ export default function index() {
 
   editor.addEventListener('input', () => render(editor.value))
   render(editor.value)
+
+  // Start marp-core brwoser observation
+  window.requestAnimationFrame(animationFrame)
 }
