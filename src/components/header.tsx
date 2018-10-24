886132553bf42df494b62957cd244de5b96950a9
Yuki Hattori <yukihattori1116@gmail.com>
2018-10-24 18:33:37 +0900

    Fix prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,13 @@ const { h } = preact
 export default () => (
   <header class={style.header}>
     <a href="https://marp.app/" target="_blank" rel="noopener">
-      <img class={style.logo} src="/marp.svg" alt="Marp" width="30" height="30" />
+      <img
+        class={style.logo}
+        src="/marp.svg"
+        alt="Marp"
+        width="30"
+        height="30"
+      />
     </a>
   </header>
 )
