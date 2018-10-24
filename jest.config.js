ed6b8526d8df9cd1c402945c847eea8ad9b0dd3a
Yuki Hattori <yukihattori1116@gmail.com>
2018-10-24 18:45:22 +0900

    Support Preact and CSS module while testing

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,16 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
   coveragePathIgnorePatterns: ['/node_modules/', '.*\\.d\\.ts'],
   coverageThreshold: { global: { lines: 95 } },
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react',
+        jsxFactory: 'h',
+      },
+    },
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: { '\\.module\\.(c|sc|sa)ss$': 'identity-obj-proxy' },
   setupFiles: ['jest-plugin-context/setup'],
   transform: jestPreset.transform,
   testEnvironment: 'jsdom',
