1bd45815ddefb9d76bcf1da69378ac0ea5206b13
Yuki Hattori <yukihattori1116@gmail.com>
2018-10-24 18:54:09 +0900

    Fix prettier

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,11 +5,17 @@ import style from './button.module.scss'
 const { h } = preact
 
 export const Button = props => (
-  <button {...props} class={`${props.class || props.className} ${style.button}`} />
+  <button
+    {...props}
+    class={`${props.class || props.className} ${style.button}`}
+  />
 )
 
 export const withClass = (klass: string, Target = Button) => props => (
-  <Target {...props} class={`${props.class || props.className} ${style[klass]}`} />
+  <Target
+    {...props}
+    class={`${props.class || props.className} ${style[klass]}`}
+  />
 )
 
 export const HeaderButton = withClass('header')
