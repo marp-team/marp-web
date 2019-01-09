2ba36ae77b54944c9c81f8b7bc6b2bc92c56f03f
Yuki Hattori <yukihattori1116@gmail.com>
2019-01-09 23:39:48 +0900

    Implement Open command

--- src/components/actions/buffer.ts
+++ src/components/actions/buffer.ts
@@ -3,8 +3,14 @@ import { GlobalStore } from '../app'
 
 interface Actions {
   newCommand: () => void
+  openCommand: () => void
   updateBuffer: (buffer: string) => void
 }
+
+const confirmBuffer = (bufferChanged?: boolean) =>
+  bufferChanged
+    ? window.confirm('Are you sure? Your changed buffer will be lost.')
+    : true
 
 export default connect<
   Partial<Pick<GlobalStore, 'buffer' | 'bufferChanged'>>,
@@ -13,13 +19,33 @@ export default connect<
   Actions
 >(
   'buffer,bufferChanged',
-  () => ({
+  store => ({
     newCommand: ({ bufferChanged }) => {
-      if (bufferChanged) {
-        if (!window.confirm('Are you sure? Your changed buffer will be lost.'))
-          return {}
-      }
+      if (!confirmBuffer(bufferChanged)) return
       return { buffer: '', bufferChanged: false }
+    },
+    openCommand: ({ bufferChanged }) => {
+      if (!confirmBuffer(bufferChanged)) return
+
+      const input = document.createElement('input')
+      input.setAttribute('type', 'file')
+      input.setAttribute(
+        'accept',
+        '.md,.mdown,.markdown,.markdn,text/markdown,text/x-markdown,text/plain'
+      )
+      input.addEventListener('change', () => {
+        const file = input.files![0]
+        const reader = new FileReader()
+
+        reader.onload = () =>
+          store.setState({
+            buffer: <string>reader.result,
+            bufferChanged: false,
+          })
+
+        reader.readAsText(file)
+      })
+      input.click()
     },
     updateBuffer: (_, buffer: string) => ({ buffer, bufferChanged: true }),
   })
