d2410002d4840117a3f4b10df003a80622da083b
Yuki Hattori <yukihattori1116@gmail.com>
2019-06-04 00:04:43 +0900

    Upgrade dependent packages to the latest version

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -12,5 +12,4 @@ declare module '*.module.scss' {
   export default cssModule
 }
 
-declare type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 declare type Overwrite<T, U> = Omit<T, Extract<keyof T, keyof U>> & U
